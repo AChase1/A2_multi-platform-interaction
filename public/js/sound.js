24333a808267ed450487ba7cb4b11fd0f5c6e8f5
aaron-chase <140291392+aaron-chase@users.noreply.github.com>
2025-01-25 18:28:40 -0500

    Instantiated electron on button click

--- public/js/sound.js
+++
@@ -1,5 +0,0 @@
-// Button click sound
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.getElementById("button");
-    button != null ? button.addEventListener("click", () => button.components.sound.playSound()) : console.log("Cannot find button");
-});
